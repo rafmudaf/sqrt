759e2aa539501582bc96e45eaeee2f23df0ccdb8
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-06-19 15:01:45 -0600

    add more integer functions

--- newtonraphson.F90
+++ newtonraphson.F90
@@ -1,6 +1,9 @@
 module newtonraphson
 implicit none
 
+private
+public :: nr_sqrt, int_2x, int_sq
+
 contains
 
 real function nr_sqrt(n, x0, iterations, printIts)
@@ -15,8 +18,9 @@ real function nr_sqrt(n, x0, iterations, printIts)
       xm1 = x0
     endif
 
-    if (mod(xm1,1.0) == 0) then
-      nr_sqrt = xm1 - (int_sq(int(xm1))-n)/(2*xm1)
+    ! if integer, use integer algorithm
+    if (isInteger(xm1)) then
+      nr_sqrt = xm1 - (int_sq(int(xm1))-n)/(int_2x(int(xm1)))
     else
       nr_sqrt = xm1 - (xm1**2-n)/(2*xm1)
     end if
@@ -31,9 +35,35 @@ real function nr_sqrt(n, x0, iterations, printIts)
   return
 end function
 
-integer function int_sq(n)
+integer function int_2x(n)
   integer, intent(in) :: n
-  int_sq = ishft(n, 1)
+  int_2x = ishft(n, 1)
+end function
+
+recursive integer function int_sq(n) result(sq)
+  integer, intent(in) :: n
+  integer :: x
+
+  if (n.eq.0) then
+    sq = 0
+  else
+    x = ishft(n, -1)
+    if (isEven(n)) then
+      sq = ishft(int_sq(x),2)
+    else
+      sq = ishft(int_sq(x),2) + ishft(x,2) + 1
+    end if
+  end if
+end function
+
+logical function isEven(n)
+  integer, intent(in) :: n
+  isEven = mod(n,2) == 0
+end function
+
+logical function isInteger(n)
+  real, intent(in) :: n
+  isInteger = mod(n,1.0) == 0
 end function
 
 end module
