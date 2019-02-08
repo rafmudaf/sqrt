3294aa63055cb4250f0801824038601d767ad8c5
Rafael M Mudafort <rafmudaf@gmail.com>
2019-02-08 15:18:31 -0700

    Add c binding to the fortran library

--- unit_test/tests/test_nr_sqrt.F90
+++ unit_test/tests/test_nr_sqrt.F90
@@ -3,5 +3,7 @@ subroutine test_nr_sqrt()
   use pFUnit_mod
   use newtonraphson
   implicit none
-  @assertEqual(2.0, nr_sqrt(4.0, 10.0, 20, .FALSE.))
+  logical(1) :: print_iterations
+  print_iterations = .FALSE.
+  @assertEqual(2.0, nr_sqrt(4.0, 10.0, 20, print_iterations))
 end subroutine test_nr_sqrt
