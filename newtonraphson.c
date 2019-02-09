74102920971a2a4032a8a2ed3ff45193da053357
Rafael M Mudafort <rafmudaf@gmail.com>
2019-02-08 17:41:57 -0700

    Add c program and library

---
+++ newtonraphson.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// enabling C99 gives us modern functionality like efficient float to int cast
+#define _ISOC9X_SOURCE 1
+#define _ISOC99_SOURCE 1
+#include <math.h>
+
+int initializer()
+{
+  // open a file for logging
+  FILE *fp;
+  fp = fopen("newtonraphson.log", "w");
+  fprintf(fp, "Testing...\n");
+  return 1;
+}
+
+bool isEven(int n)
+{
+  return n % 2 == 0;
+}
+
+int int_sq(int n)
+{
+  int x, sq;
+  if (n == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    x = n >> 1;
+    if (isEven(n))
+    {
+      return int_sq(x) << 2;
+    }
+    else
+    {
+      return (int_sq(x) << 2) + (x << 2) + 1;
+    }
+  }
+}
+
+int int_2x(int n)
+{
+  return n << 1;
+}
+
+bool isInteger(double n)
+{
+  return fmod(n, 1.0) == 0;
+}
+
+double nr_sqrt(double *n, double *x0, int *iterations, bool *printIts)
+{
+  double xm1, root;
+
+  for (int i = 0; i < *iterations; ++i)
+  {
+    if (i == 0)
+    {
+      xm1 = *x0;
+    }
+
+    // if integer, use integer algorithm
+    if (isInteger(xm1))
+    {
+      root = xm1 - (int_sq(lrint(xm1)) - *n) / (int_2x(lrint(xm1)));
+    }
+    else
+    {
+      root = xm1 - (pow(xm1, 2) - *n) / (2 * xm1);
+    }
+
+    if (printIts)
+    {
+      printf("at iteration %i - %f\n", i, xm1);
+    }
+
+    xm1 = root;
+  }
+
+  return root;
+}
