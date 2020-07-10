7b839747f7d2bcf51fd78f0e8d72077a5f55259f
Rafael M Mudafort <rafmudaf@gmail.com>
2020-07-10 12:24:19 -0500

    C-Fortran array passinghttps://gcc.gnu.org/onlinedocs/gcc-4.9.2/gfortran/Working-with-Pointers.htmlhttps://www.tutorialspoint.com/fortran/fortran_pointers.htmhttps://gcc.gnu.org/onlinedocs/gfortran/C_005fF_005fPOINTER.html

--- unit_test.py
+++ unit_test.py
@@ -1,7 +1,16 @@
 
 import sys
 import numpy as np
-from ctypes import CDLL, POINTER, c_int, c_double
+from ctypes import (
+    CDLL,
+    POINTER,
+    c_int,
+    c_float,
+    c_double,
+    Structure,
+    pointer,
+    byref
+)
 
 if sys.platform == "darwin":
     library_extension = "dylib"
@@ -15,17 +24,37 @@ elif sys.platform == "win32":
 
 newtonraphsonlib = CDLL('./build/libnewtonraphson_fortran.{}'.format(library_extension))
 
-def test_int_2x():
-    desired = 8
-    actual = newtonraphsonlib.int_2x(4)
-    np.testing.assert_almost_equal(actual, desired)
+class array_container(Structure):
+    # _pack_ = True # source:False
+    _fields_ = [
+        ('array', POINTER(c_float)),
+        ('array_len', c_int)
+    ]
 
+if __name__ == "__main__":
 
-def test_int_sq():
-    desired = 16
-    actual = newtonraphsonlib.int_sq(4)
-    np.testing.assert_almost_equal(actual, desired)
+    newtonraphsonlib.array_passing.argtypes = [
+        POINTER(array_container),
+        POINTER(array_container)
+    ]
+    newtonraphsonlib.array_passing.restypes = c_int
 
-if __name__=="__main__":
-    test_int_2x()
-    test_int_sq()
+    n_inputs = 6
+    n_outputs = 6
+    array_in = array_container(
+        (n_inputs * c_float)(1.1, 2.2, 3.3, 4.4, 5.5, 6.6),
+        c_int(n_inputs)
+    )
+
+    array_out = array_container()
+
+    print("Calling Fortran library...")
+    return_code = newtonraphsonlib.array_passing(
+        byref(array_in),
+        byref(array_out)
+    )
+    print(f"Returned with response code: {return_code}")
+
+    print(array_out.array_len)
+    for i in range(n_outputs):
+        print(f"{i}: {array_out.array[i]}")
