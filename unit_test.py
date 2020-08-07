58c43324b6f6ec0fed35fdbab2484e075dc0a061
Rafael M Mudafort <rafmudaf@gmail.com>
2020-08-07 17:48:22 -0500

    Add support for 2D arrays

--- unit_test.py
+++ unit_test.py
@@ -31,6 +31,15 @@ class array_container(Structure):
         ('array_len', c_int)
     ]
 
+
+class array2d_container(Structure):
+    # _pack_ = True # source:False
+    _fields_ = [
+        ('array', POINTER(array_container)),
+        ('array_len', c_int)
+    ]
+
+
 if __name__ == "__main__":
 
     newtonraphsonlib.array_passing.argtypes = [
@@ -39,6 +48,13 @@ if __name__ == "__main__":
     ]
     newtonraphsonlib.array_passing.restypes = c_int
 
+    newtonraphsonlib.array_passing2d.argtypes = [
+        POINTER(array2d_container),
+        POINTER(array2d_container)
+    ]
+    newtonraphsonlib.array_passing2d.restypes = c_int
+
+    # Pass a single array and get it back
     n_inputs = 6
     n_outputs = 6
     array_in = array_container(
@@ -58,3 +74,32 @@ if __name__ == "__main__":
     print(array_out.array_len)
     for i in range(n_outputs):
         print(f"{i}: {array_out.array[i]}")
+
+
+    # Nested arrays - array of arrays
+    array_temp1 = array_container(
+        (n_inputs * c_float)(1.1, 2.2, 3.3, 4.4, 5.5, 6.6),
+        c_int(n_inputs)
+    )
+    array_temp2 = array_container(
+        (n_inputs * c_float)(7.7, 8.8, 9.9, 11.0, 12.1, 13.2),
+        c_int(n_inputs)
+    )
+    array_in = array2d_container(
+        (2 * array_container)(array_temp1, array_temp2),
+        c_int(2)
+    )
+
+    array_out = array2d_container()
+
+    print("Calling Fortran library...")
+    return_code = newtonraphsonlib.array_passing2d(
+        byref(array_in),
+        byref(array_out)
+    )
+    print(f"Returned with response code: {return_code}")
+
+    print(array_out.array_len)
+    for j in range(2):
+        for i in range(n_outputs):
+            print(f"{i}: {array_out.array[j].array[i]}")
