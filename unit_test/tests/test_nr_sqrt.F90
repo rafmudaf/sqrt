58401831a4b07a5f96fe73b5af10e8d3f21dc82c
Rafael M Mudafort <rafmudaf@gmail.com>
2021-03-05 16:16:35 -0600

    Bug fix: Update data type in unit test

--- unit_test/tests/test_nr_sqrt.F90
+++ unit_test/tests/test_nr_sqrt.F90
@@ -2,8 +2,10 @@
 subroutine test_nr_sqrt()
   use pFUnit_mod
   use newtonraphson
+  use iso_c_binding
+
   implicit none
   logical(1) :: print_iterations
   print_iterations = .FALSE.
-  @assertEqual(2.0, nr_sqrt(4.0, 10.0, 20, print_iterations))
+  @assertEqual(2.0, nr_sqrt(real(4.0, c_double), real(10.0, c_double), 20, print_iterations))
 end subroutine test_nr_sqrt
