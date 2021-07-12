8bd27ab5011d6b4d810d6564a017b143a8178822
Rafael M Mudafort <rafmudaf@gmail.com>
2021-07-12 18:38:24 -0500

    Generate c prototypes with compiler flag

--- sqrt_f90.cpp
+++ sqrt_f90.cpp
@@ -3,6 +3,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+
+#include <stddef.h>
+#ifdef __cplusplus
+#include <complex>
+#define __GFORTRAN_FLOAT_COMPLEX std::complex<float>
+#define __GFORTRAN_DOUBLE_COMPLEX std::complex<double>
+#define __GFORTRAN_LONG_DOUBLE_COMPLEX std::complex<long double>
+extern "C" {
+#else
+#define __GFORTRAN_FLOAT_COMPLEX float _Complex
+#define __GFORTRAN_DOUBLE_COMPLEX double _Complex
+#define __GFORTRAN_LONG_DOUBLE_COMPLEX long double _Complex
+#endif
+
+typedef struct array2d_container_c {
+    void *array;
+    int array_len /* WARNING: Converting 'INTEGER(4)' to interoperable type */;
+} array2d_container_c;
+typedef struct array_container_c {
+    void *array;
+    int array_len /* WARNING: Converting 'INTEGER(4)' to interoperable type */;
+} array_container_c;
+void array_passing (const array_container_c *input_data, array_container_c *output_data, const char *string);
+void array_passing2d (const array2d_container_c *input_data, array2d_container_c *output_data);
+int int_2x (int n);
+int int_sq (int n);
+double nr_sqrt (const double *n, const double *x0, const int *iterations, const _Bool *printits);
+
+#ifdef __cplusplus
+}
+#endif
+
 extern "C"
 {
     double nr_sqrt(double *, double *, int *, bool *);
